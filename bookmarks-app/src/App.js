cd344824ba2d3dcb615e8d5def37b69a040ef8f4
AveraqeDev <averaqedev@gmail.com>
2020-02-29 21:37:39 -0800

    start working on bookmarks client

--- bookmarks-app/src/App.js
+++ bookmarks-app/src/App.js
@@ -5,46 +5,16 @@ import Nav from './Nav/Nav';
 import config from './config';
 import './App.css';
 
-const bookmarks = [
-  // {
-  //   id: 0,
-  //   title: 'Google',
-  //   url: 'http://www.google.com',
-  //   rating: '3',
-  //   desc: 'Internet-related services and products.'
-  // },
-  // {
-  //   id: 1,
-  //   title: 'Thinkful',
-  //   url: 'http://www.thinkful.com',
-  //   rating: '5',
-  //   desc: '1-on-1 learning to accelerate your way to a new high-growth tech career!'
-  // },
-  // {
-  //   id: 2,
-  //   title: 'Github',
-  //   url: 'http://www.github.com',
-  //   rating: '4',
-  //   desc: 'brings together the world\'s largest community of developers.'
-  // }
-];
-
 class App extends Component {
   state = {
-    page: 'list',
-    bookmarks,
-    error: null,
+    bookmarks: [],
+    error: null
   };
-
-  changePage = (page) => {
-    this.setState({ page })
-  }
 
   setBookmarks = bookmarks => {
     this.setState({
       bookmarks,
       error: null,
-      page: 'list',
     })
   }
 
